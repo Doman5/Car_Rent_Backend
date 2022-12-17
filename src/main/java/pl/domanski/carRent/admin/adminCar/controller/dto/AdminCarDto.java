6fac04ce06b803599e809a10df783af50dca905b
Dominik <dono3108@gmail.com>
2022-12-17 21:12:42 +0100

    add validation for AdminCarDto

--- src/main/java/pl/domanski/carRent/admin/adminCar/controller/dto/AdminCarDto.java
+++ src/main/java/pl/domanski/carRent/admin/adminCar/controller/dto/AdminCarDto.java
@@ -3,12 +3,19 @@ package pl.domanski.carRent.admin.adminCar.controller.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+
 @Getter
 @Builder
 public class AdminCarDto {
     private Long id;
+    @NotBlank
     private String brand;
+    @NotBlank
     private String model;
+    @Min(0)
     private int year;
     private AdminCarTechnicalSpecificationDto adminCarTechnicalSpecificationDto;
 }
