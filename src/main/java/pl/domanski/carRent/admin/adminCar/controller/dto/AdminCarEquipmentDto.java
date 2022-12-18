ae3b2fe809fe67aeb715525bea35b9a24a8954de
Dominik <dono3108@gmail.com>
2022-12-18 09:14:27 +0100

    correct AdminCarServiceTest

--- src/main/java/pl/domanski/carRent/admin/adminCar/controller/dto/AdminCarEquipmentDto.java
+++ src/main/java/pl/domanski/carRent/admin/adminCar/controller/dto/AdminCarEquipmentDto.java
@@ -1,8 +1,10 @@
 package pl.domanski.carRent.admin.adminCar.controller.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class AdminCarEquipmentDto {
     private String name;
 }
--- src/test/java/pl/domanski/carRent/admin/adminCar/service/AdminCarServiceTest.java
+++ src/test/java/pl/domanski/carRent/admin/adminCar/service/AdminCarServiceTest.java
@@ -6,6 +6,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.domanski.carRent.admin.adminCar.controller.dto.AdminCarDto;
+import pl.domanski.carRent.admin.adminCar.controller.dto.AdminCarEquipmentDto;
 import pl.domanski.carRent.admin.adminCar.controller.dto.AdminCarTechnicalSpecificationDto;
 import pl.domanski.carRent.admin.adminCar.model.AdminCar;
 import pl.domanski.carRent.admin.adminCar.model.AdminCarEquipment;
@@ -43,9 +44,9 @@ class AdminCarServiceTest {
         AdminCarDto adminCarDto = createAdminCarDto();
         given(adminCarRepository.findById(carId)).willReturn(Optional.of(createTestAdminCar()));
         given(adminCarEquipmentRepository.findAllByCarId(carId))
-                .willReturn(List.of());
+                .willReturn(createEquipmentList());
         given(adminCarTechSpecRepository.save(any())).willReturn(createAdminTechSpec());
-        given(adminCarRepository.save(any())).willReturn(createAdminCar());
+        given(adminCarRepository.save(any())).willAnswer(invocationOnMock -> invocationOnMock.getArguments()[0]);
         //when
         AdminCar result = adminCarService.updateCar(adminCarDto, 1L);
         //then
@@ -56,6 +57,14 @@ class AdminCarServiceTest {
         assertEquals(200, result.getAdminCarTechnicalSpecification().getPower());
         assertEquals(2L, result.getAdminCarTechnicalSpecification().getId());
         assertThat(result.getEquipments(), hasSize(3));
+        assertEquals(3L, result.getEquipments().get(2).getId());
+    }
+
+    private List<AdminCarEquipment> createEquipmentList() {
+        return List.of(AdminCarEquipment.builder().id(1L).name("Klimatyzacja").build(),
+                AdminCarEquipment.builder().id(2L).name("Pakiet Carbon").build(),
+                AdminCarEquipment.builder().id(3L).name("S-Line").build()
+        );
     }
 
     private static AdminCar createTestAdminCar() {
@@ -91,32 +100,12 @@ class AdminCarServiceTest {
                                 .gearbox("automatic")
                                 .fuel("diesel")
                                 .seats("5")
-                                .build()
-                )
+                                .build())
+                .carEquipments(List.of(AdminCarEquipmentDto.builder().name("Klimatyzacja").build(),
+                        AdminCarEquipmentDto.builder().name("Pakiet Carbon").build(),
+                        AdminCarEquipmentDto.builder().name("S-Line").build()
+                ))
                 .build();
     }
 
-    private AdminCar createAdminCar() {
-        return AdminCar.builder()
-                .id(1L)
-                .brand("Audi")
-                .model("A4")
-                .year(2010)
-                .adminCarTechnicalSpecification(
-                        AdminCarTechnicalSpecification.builder()
-                                .id(2L)
-                                .power(200)
-                                .engine("R4 2.0")
-                                .drive("FWD")
-                                .acceleration("7.2")
-                                .gearbox("automatic")
-                                .fuel("diesel")
-                                .seats("5")
-                                .build()
-                )
-                .equipments(List.of(AdminCarEquipment.builder().build(),
-                        AdminCarEquipment.builder().build(),
-                        AdminCarEquipment.builder().build()))
-                .build();
-    }
 }
